61bb39ece7a603a8c66e0a53e3cf69150610de72
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-09-13 17:45:23 +0530

    Create Orders Fragment Changes + Create Fragment UI Changes

--- app/src/main/java/com/example/procurement/utils/CommonConstants.java
+++ app/src/main/java/com/example/procurement/utils/CommonConstants.java
@@ -8,6 +8,7 @@ public class CommonConstants {
     public static final int ORDER_STATUS_PLACED = 2;
     public static final int ORDER_STATUS_HOLD = 3;
 
+    public static final String FIREBASE_DATABASE_NAME = "Orders";
     public static final String VIEW_STATUS_ORDER_EXTRA = "VIEW_STATUS_ORDER_EXTRA";
     public static final String VIEW_STATUS_ORDER_POSITION = "VIEW_STATUS_ORDER_POSITION";
 
