67245c28b0da6af2b807a1639cc70a47600563ce
Praveenkumar <pk21.praveen@gmail.com>
2019-09-16 21:00:46 +0530

    fb changes 0.3

--- app/src/main/java/com/example/procurement/PMS.java
+++ app/src/main/java/com/example/procurement/PMS.java
@@ -8,15 +8,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class PMS extends Application {
-    public static DatabaseReference orderDatabaseRef, NotesDatabaseRef, NotificationDatabaseRef;
+    public static DatabaseReference DatabaseRef;
 
     @Override
     public void onCreate() {
         super.onCreate();
         SignInActivity.getCurrentUser();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        orderDatabaseRef = database.getReference(CommonConstants.USER_DB_URL + CommonConstants.FIREBASE_ORDER_DB);
-        NotesDatabaseRef = database.getReference(CommonConstants.USER_DB_URL + CommonConstants.FIREBASE_NOTES_DB);
-        NotificationDatabaseRef = database.getReference(CommonConstants.USER_DB_URL + CommonConstants.FIREBASE_NOTIFICATION_DB);
+        DatabaseRef = database.getReference(CommonConstants.USER_DB_URL);
     }
 }
