74bb3ccb48f76ebd5736248d617ea3f90571d036
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-09-23 23:27:42 +0530

    Order View Small Update Changes

--- app/src/main/java/com/example/procurement/fragments/OrderViewFragment.java
+++ app/src/main/java/com/example/procurement/fragments/OrderViewFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.procurement.PMS;
 import com.example.procurement.R;
+import com.example.procurement.activities.HomeActivity;
 import com.example.procurement.models.Order;
 import com.example.procurement.utils.CommonConstants;
 import com.google.firebase.database.DataSnapshot;
@@ -28,7 +30,8 @@ public class OrderViewFragment extends Fragment {
     private String orderKey;
     private Order order;
     private DatabaseReference orderDatabaseRef;
-    private TextView textView;
+    private ImageView btnBack;
+    private TextView txtOrderNameTitle,txtOrderIdView,txtOrderNameView,dtpDateView,txtDescriptionView,txtStatusView;
 
     public OrderViewFragment(String orderKey) {
         this.orderKey = orderKey;
@@ -42,10 +45,28 @@ public class OrderViewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_order_view, container, false);
 
         orderDatabaseRef = PMS.DatabaseRef.child(CommonConstants.FIREBASE_ORDER_DB).child(orderKey).getRef();
-
-        textView = view.findViewById(R.id.orderView);
+        btnBack = view.findViewById(R.id.btnBack);
+        txtOrderNameTitle = view.findViewById(R.id.txtOrderNameTitle);
+        txtOrderIdView = view.findViewById(R.id.txtOrderIdView);
+        txtOrderNameView = view.findViewById(R.id.txtOrderNameView);
+        dtpDateView = view.findViewById(R.id.dtpDateView);
+        txtDescriptionView = view.findViewById(R.id.txtDescription);
+        txtStatusView = view.findViewById(R.id.txtStatusView);
         readStatusData();
+        getBack();
+
         return view;
+    }
+
+    private void getBack() {
+        btnBack.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        HomeActivity.fm.beginTransaction().replace(R.id.fragment_container,new OrderStatusFragment(),null).commit();
+                    }
+                }
+        );
     }
 
     private void readStatusData() {
@@ -55,7 +76,12 @@ public class OrderViewFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 order = new Order();
                 order = dataSnapshot.getValue(Order.class);
-                textView.setText(order.getOrderID() + "\n " + order.getDescription());
+                txtOrderNameTitle.setText(order.getOrderID() + " - "+order.getName());
+                txtOrderIdView.setText(order.getOrderID());
+                txtOrderNameView.setText(order.getName());
+                txtDescriptionView.setText(order.getDescription());
+                dtpDateView.setText(order.getDate());
+                txtStatusView.setText(order.getStatus());
             }
 
             @Override
