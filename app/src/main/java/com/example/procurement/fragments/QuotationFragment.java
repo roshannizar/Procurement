d6be9e61ce13ca48122ef83ad49de3c614bf0784
Roshan Nizar <41587533+roshannizar@users.noreply.github.com>
2019-09-26 00:22:19 +0530

    Requisition Create Partially Completed

--- app/src/main/java/com/example/procurement/fragments/QuotationFragment.java
+++ app/src/main/java/com/example/procurement/fragments/QuotationFragment.java
@@ -3,23 +3,39 @@ package com.example.procurement.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.procurement.R;
 import com.example.procurement.activities.RequisitionActivity;
+import com.example.procurement.models.Order;
+import com.example.procurement.models.Requisition;
+import com.example.procurement.utils.CommonConstants;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+import static com.example.procurement.activities.SignInActivity.siteManagerDBRef;
 
 public class QuotationFragment extends Fragment {
 
     private Button btnPlacedRequisition,btnBackRequisition;
+    private EditText txtReason;
+    private TextView txtProposalDate,txtProposedBy;
+    private CollectionReference requisitionRef;
 
     public QuotationFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -31,11 +47,36 @@ public class QuotationFragment extends Fragment {
 
         btnPlacedRequisition = v.findViewById(R.id.btnPlaceRequisition2);
         btnBackRequisition = v.findViewById(R.id.btnBackRequisition);
-
+        txtReason = v.findViewById(R.id.txtReason);
+        txtProposalDate = v.findViewById(R.id.txtProposalDate);
+        txtProposedBy = v.findViewById(R.id.txtProposedBy);
+        requisitionRef = siteManagerDBRef.collection(CommonConstants.COLLECTION_REQUISITION);
         PlaceOrder();
         GoToRequisition();
 
         return v;
+    }
+
+    private void WriteStatus() {
+        String key = requisitionRef.document().getId();
+        Requisition requisition = new Requisition(RequisitionActivityFragment.REQUISITION_NO, RequisitionActivityFragment.COMMENTS, RequisitionActivityFragment.PURPOSE, RequisitionActivityFragment.DELIVERY_DATE, RequisitionActivityFragment.TOTAL_AMOUNT,CommonConstants.ORDER_STATUS_PENDING,txtReason.getText().toString(),txtProposalDate.getText().toString(),txtProposedBy.getText().toString());
+        requisition.setKey(key);
+        requisitionRef.document(key)
+                .set(requisition)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getContext(),"Requisition placed successfully!",Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "Requisition placed successfully!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(),"Error while placing the document",Toast.LENGTH_LONG).show();
+                        Log.w(TAG, "Error while placing the document", e);
+                    }
+                });
     }
 
     private void PlaceOrder() {
@@ -43,13 +84,13 @@ public class QuotationFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(getActivity(),"Created Successfully!",Toast.LENGTH_LONG).show();
+                        WriteStatus();
                     }
                 }
         );
     }
 
-    public void GoToRequisition() {
+    private void GoToRequisition() {
         btnBackRequisition.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
--- app/src/main/java/com/example/procurement/fragments/RequisitionActivityFragment.java
+++ app/src/main/java/com/example/procurement/fragments/RequisitionActivityFragment.java
@@ -7,14 +7,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.procurement.R;
 import com.example.procurement.activities.RequisitionActivity;
+import com.google.firestore.v1.Write;
 
 public class RequisitionActivityFragment extends Fragment {
 
     private Button btnQuotation;
-
+    private EditText txtRequisitionNo,txtPurpose,txtComments;
+    private TextView txtDeliveryDate,txtTotalAmount;
+    public static String REQUISITION_NO,PURPOSE,COMMENTS,DELIVERY_DATE,TOTAL_AMOUNT;
 
     public RequisitionActivityFragment() {
     }
@@ -25,6 +30,11 @@ public class RequisitionActivityFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_requisition, container, false);
 
         btnQuotation = v.findViewById(R.id.btnQuotation);
+        txtRequisitionNo = v.findViewById(R.id.txtRequisitionNo);
+        txtPurpose = v.findViewById(R.id.txtPurpose);
+        txtComments = v.findViewById(R.id.txtComments);
+        txtDeliveryDate = v.findViewById(R.id.txtDeliveryDate);
+        txtTotalAmount = v.findViewById(R.id.txtTotalAmount);
 
         GoToQuotation();
 
@@ -36,6 +46,14 @@ public class RequisitionActivityFragment extends Fragment {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+
+                        //REQUISITION_NO = txtRequisitionNo.getText().toString();
+                        REQUISITION_NO = "REQ-1809";
+                        PURPOSE = txtPurpose.getText().toString();
+                        COMMENTS = txtComments.getText().toString();
+                        DELIVERY_DATE = txtDeliveryDate.getText().toString();
+                        TOTAL_AMOUNT = txtTotalAmount.getText().toString();
+
                         RequisitionActivity.fm2.beginTransaction().replace(R.id.fragment_container_requisition, new QuotationFragment(), null).commit();
                     }
                 }
