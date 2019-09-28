f934f8accbff557f16449908a43cc67617811dfc
Praveenkumar <pk21.praveen@gmail.com>
2019-09-28 23:34:21 +0530

    Update QuotationFragment.java

--- app/src/main/java/com/example/procurement/fragments/QuotationFragment.java
+++ app/src/main/java/com/example/procurement/fragments/QuotationFragment.java
@@ -67,7 +67,7 @@ public class QuotationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_quotation, container, false);
 
