95a6f28090d57c086f276d190ccaf66823f53460
Praveenkumar <pk21.praveen@gmail.com>
2019-09-29 11:53:36 +0530

    Update RequisitionStatusTest.java

--- app/src/test/java/com/example/procurement/RequisitionStatusTest.java
+++ app/src/test/java/com/example/procurement/RequisitionStatusTest.java
@@ -1,10 +1,9 @@
 package com.example.procurement;
 
-import com.example.procurement.filters.purchaseOrder.ApprovedOrderStatus;
-import com.example.procurement.filters.purchaseOrder.OrderStatus;
 import com.example.procurement.filters.requisition.ApprovedRequisitionStatus;
+import com.example.procurement.filters.requisition.HoldRequisitionStatus;
+import com.example.procurement.filters.requisition.PendingRequisitionStatus;
 import com.example.procurement.filters.requisition.RequisitionStatus;
-import com.example.procurement.models.Order;
 import com.example.procurement.models.Requisition;
 
 import org.junit.Test;
@@ -51,6 +50,82 @@ public class RequisitionStatusTest {
 
         int actual = approved.meetRequisitionStatus(requisitions).size();
 
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void FilterPendingStatus() {
+
+        RequisitionStatus pending = new PendingRequisitionStatus();
+        List<Requisition> requisitions = new ArrayList<>();
+
+        Requisition requisition = new Requisition("Req-001","Extension","Future Use","12/12/2019","12,500","Pending","Discounts","12/12/2019","Roshan Nizar","Yes");
+        Requisition requisition1 = new Requisition("Req-002","New Building","Not Enough space","13/12/2019","125,000","Pending","Discounts","12/12/2019","Roshan Nizar","Yes");
+
+        requisitions.add(requisition);
+        requisitions.add(requisition1);
+
+        pending.meetRequisitionStatus(requisitions);
+
+        int actual = pending.meetRequisitionStatus(requisitions).size();
+
         assertEquals(2, actual);
+    }
+
+    @Test
+    public void FilterNotPendingStatus() {
+
+        RequisitionStatus pending = new PendingRequisitionStatus();
+        List<Requisition> requisitions = new ArrayList<>();
+
+        Requisition requisition = new Requisition("Req-001","Extension","Future Use","12/12/2019","12,500","Pending","Discounts","12/12/2019","Roshan Nizar","Yes");
+        Requisition requisition1 = new Requisition("Req-002","New Building","Not Enough space","13/12/2019","125,000","Hold","Discounts","12/12/2019","Roshan Nizar","Yes");
+
+        requisitions.add(requisition);
+        requisitions.add(requisition1);
+
+        pending.meetRequisitionStatus(requisitions);
+
+        int actual = pending.meetRequisitionStatus(requisitions).size();
+
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void FilterHoldStatus() {
+
+        RequisitionStatus hold = new HoldRequisitionStatus();
+        List<Requisition> requisitions = new ArrayList<>();
+
+        Requisition requisition = new Requisition("Req-001","Extension","Future Use","12/12/2019","12,500","Hold","Discounts","12/12/2019","Roshan Nizar","Yes");
+        Requisition requisition1 = new Requisition("Req-002","New Building","Not Enough space","13/12/2019","125,000","Hold","Discounts","12/12/2019","Roshan Nizar","Yes");
+
+        requisitions.add(requisition);
+        requisitions.add(requisition1);
+
+        hold.meetRequisitionStatus(requisitions);
+
+        int actual = hold.meetRequisitionStatus(requisitions).size();
+
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void FilterNotHoldStatus() {
+
+        RequisitionStatus hold = new HoldRequisitionStatus();
+        List<Requisition> requisitions = new ArrayList<>();
+
+        Requisition requisition = new Requisition("Req-001","Extension","Future Use","12/12/2019","12,500","Pending","Discounts","12/12/2019","Roshan Nizar","Yes");
+        Requisition requisition1 = new Requisition("Req-002","New Building","Not Enough space","13/12/2019","125,000","Hold","Discounts","12/12/2019","Roshan Nizar","Yes");
+
+        requisitions.add(requisition);
+        requisitions.add(requisition1);
+
+        hold.meetRequisitionStatus(requisitions);
+
+        int actual = hold.meetRequisitionStatus(requisitions).size();
+
+        assertEquals(1, actual);
     }
 }
